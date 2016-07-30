bb939be2711e741df029e40dfad6024e52c0c8c5
Ken Sonoda <ken.soda0@gmail.com>
2016-07-30 09:49:22 +0900

    Add android

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,5 +1,6 @@
 HASKELLPATH=~/.local
 #PEBBLEPATH=~/pebble-dev/PebbleSDK-3.3/bin
+ANDROIDPATH=/usr/local/android-studio/bin
 RBENVPATH=~/.rbenv
 export GOPATH=~/gocode
-export PATH="$RBENVPATH/bin:$GOPATH/bin:$HASKELLPATH/bin:$PATH"
+export PATH="$RBENVPATH/bin:$GOPATH/bin:$HASKELLPATH/bin:$ANDROIDPATH:$PATH"
