8188b8b9cb6ca72c3075f477b07d8aa3c366618e
Ken Sonoda <ken.soda0@gmail.com>
2016-08-13 23:31:03 +0900

    update

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -2,5 +2,6 @@ HASKELLPATH=~/.local
 #PEBBLEPATH=~/pebble-dev/PebbleSDK-3.3/bin
 ANDROIDPATH=/usr/local/android-studio/bin
 RBENVPATH=~/.rbenv
+export ANDROID_HOME=~/Android/Sdk
 export GOPATH=~/gocode
 export PATH="$RBENVPATH/bin:$GOPATH/bin:$HASKELLPATH/bin:$ANDROIDPATH:$PATH"
