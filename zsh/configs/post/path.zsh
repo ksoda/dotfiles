e100bf74f65a8741fe45d2c792c200be60c3726b
Ken Sonoda <ken.soda0@gmail.com>
2016-11-14 02:38:58 +0900

    :fire: g fn

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,7 +1,4 @@
 HASKELLPATH=~/.local
-PEBBLEPATH=~/pebble-dev/pebble-sdk-4.5-linux64
-ANDROIDPATH=/usr/local/android-studio
 RBENVPATH=~/.rbenv
-export ANDROID_HOME=~/Android/Sdk
 export GOPATH=~/go
-export PATH="$PEBBLEPATH/bin:$RBENVPATH/bin:$GOPATH/bin:$HASKELLPATH/bin:$PATH"
+export PATH="$RBENVPATH/bin:$GOPATH/bin:$HASKELLPATH/bin:$PATH"
