ac903c617fcca5ac8f483373159dd6c7e0e06e53
Ken Sonoda <ken.soda0@gmail.com>
2016-12-10 00:40:54 +0900

    autosuggestions

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -8,3 +8,6 @@ compinit
 autoload -U +X compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
 eval "$(stack --bash-completion-script stack)"
+
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
