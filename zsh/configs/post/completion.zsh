2a9a2852137f04b155431c7b27b3c9c8722c3ea7
Ken Sonoda <ken.soda0@gmail.com>
2018-01-12 00:01:00 +0900

    Add completion

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -4,6 +4,11 @@ fpath=(~/.zsh/completion /usr/local/share/zsh/site-functions $fpath)
 # completion
 autoload -U +X compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
-eval "$(stack --bash-completion-script stack)"
+command -v stack >/dev/null 2>&1 || {
+    eval "$(stack --bash-completion-script stack)"
+}
 compdef '_files -g "*.hs"' stack
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+if [ -e /usr/share/bash-completion/completions/lxc ]
+then source /usr/share/bash-completion/completions/lxc
+fi
