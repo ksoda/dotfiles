9dda0e1c2c5c3cc63ea692aacc9aa37afbeb50ea
Ken Sonoda <ken.soda0@gmail.com>
2018-05-06 02:07:52 +0900

    Update

--- config/fish/conf.d/variables.fish
+++ config/fish/conf.d/variables.fish
@@ -3,10 +3,7 @@ alias e $EDITOR
 set -x LESS "-R"
 set -x LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 set -x PATH $HOME/go/bin $PATH
+set -x PATH $HOME/.local/bin $PATH
 
-# ANDROID_HOME="$HOME/Android/Sdk"
 # CARGOPATH="$HOME/.cargo"
-# HASKELLPATH="$HOME/.local"
 # NODEPATH="$HOME/.npm-global"
-# PYTHONSTARTUP="$HOME/.pythonrc"
-# RBENVPATH="$HOME/.rbenv"
