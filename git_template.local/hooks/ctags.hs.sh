f7500420ed10b9e76abe22595430cba414020d72
Ken Sonoda <ken.soda0@gmail.com>
2018-06-03 21:50:10 +0900

    Update

--- git_template.local/hooks/ctags.hs.sh
+++ git_template.local/hooks/ctags.hs.sh
@@ -5,8 +5,9 @@ set -e
 PATH="/usr/local/bin:$PATH"
 dir="$(git rev-parse --git-dir)"
 trap 'rm -f "$dir/$$.tags"' EXIT
-hasktags --ctags --ignore-close-implementation \
-  --tags-absolute --output="$dir/$$.tags" .
+
+git ls-files '*.hs' | \
+  xargs hasktags --ctags --file="$dir/$$.tags"
 
 root_dir="$(git rev-parse --show-toplevel)"
 mv "$dir/$$.tags" "$root_dir/.tags"
