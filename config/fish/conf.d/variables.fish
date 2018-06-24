8a8ba83eb33d56693ef43e5311898edf49e85099
Ken Sonoda <ken.soda0@gmail.com>
2018-06-25 01:39:26 +0900

    NPM

--- config/fish/conf.d/variables.fish
+++ config/fish/conf.d/variables.fish
@@ -1,9 +1,9 @@
-set -U EDITOR vim
+set -x $EDITOR vim
 alias e $EDITOR
 set -x LESS "-R"
 set -x LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 set -x PATH $HOME/go/bin $PATH
 set -x PATH $HOME/.local/bin $PATH
+set -x PATH $HOME/.npm-global/bin $PATH
+set -x PATH $HOME/.cargo/bin $PATH
 
-# CARGOPATH="$HOME/.cargo"
-# NODEPATH="$HOME/.npm-global"
