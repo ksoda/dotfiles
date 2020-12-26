0280360556556a9ef7d1aba5105d096db1eca9df
Ken Sonoda <ksoda@users.noreply.github.com>
2020-12-26 17:29:36 +0900

    update

--- config/fish/conf.d/variables.fish
+++ config/fish/conf.d/variables.fish
@@ -1,10 +1,6 @@
 set -x EDITOR vim
-alias e=$EDITOR
 set -x LESS "-R"
 set -x LESSOPEN "| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
 set -x sudope_sequence \cs
-# set -x PATH $PATH $GOPATH/bin
-# set -x PATH $PATH $HOME/.volta/bin
-# set -x PATH $PATH $HOME/.cargo/bin
-# set -x PATH $PATH $HOME/.rbenv/bin
 
+alias e=$EDITOR
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,3 +6,7 @@ end
 
 source /home/ken/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 eval (direnv hook fish)
+
+# ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+test -f /home/ken/.ghcup/env ; and set -gx PATH $HOME/.cabal/bin /home/ken/.ghcup/bin $PATH
