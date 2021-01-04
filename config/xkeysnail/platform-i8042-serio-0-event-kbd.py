5b7ebd1ca13d22ea7e714bee6167154c95269e0f
Ken Sonoda <ksoda@users.noreply.github.com>
2021-01-04 20:03:18 +0900

    update

--- config/xkeysnail/platform-i8042-serio-0-event-kbd.py
+++ config/xkeysnail/platform-i8042-serio-0-event-kbd.py
@@ -5,10 +5,12 @@ define_modmap({
     Key.BACKSLASH: Key.BACKSPACE,
     Key.BACKSPACE: Key.BACKSLASH,
     Key.SYSRQ: Key.RIGHT_META,
-    Key.CAPSLOCK: Key.ESC
+    Key.CAPSLOCK: Key.ESC,
+    Key.LEFT_SHIFT: Key.LEFT,
+    Key.RIGHT_SHIFT: Key.RIGHT,
 })
 
 define_multipurpose_modmap({
-    Key.SPACE: [Key.SPACE, Key.RIGHT_CTRL],
-    Key.LEFT_ALT: [Key.HENKAN, Key.LEFT_ALT]
+    Key.SPACE: [Key.SPACE, Key.LEFT_SHIFT],
+    Key.RIGHT_ALT: [Key.HENKAN, Key.RIGHT_CTRL],
 })
